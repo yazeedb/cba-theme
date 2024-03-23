6f1d03bc12ef35601f74f9f968ed1d0553e320cb
Yazeed Bzadough <ybzadough@gmail.com>
2024-03-23 16:41:57 -0400

    Remove Astro/Netlify integration, since static site does not need it

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  adapter: netlify()
+  integrations: [tailwind(), react()]
 });
