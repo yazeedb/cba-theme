8635ad536973738cf293839386344f4f80d04475
Yazeed Bzadough <ybzadough@gmail.com>
2024-04-20 16:54:04 -0400

    Get all members

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -1,7 +1,7 @@
 const baseRoute = import.meta.env.PUBLIC_WP_API;
 
 export const routes = {
-  boardMembers: `${baseRoute}/board_members`,
+  boardMembers: `${baseRoute}/board_members?per_page=100`,
   events: `${baseRoute}/acf-custom-event?_embed`,
   pageSpoilers: `${baseRoute}/page-spoiler`,
   carouselAds: `${baseRoute}/carousel-ad?_embed`,
