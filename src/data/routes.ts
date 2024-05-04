4e8d4dca084a33c325bfd0dbb83c77781c4b3525
Yazeed Bzadough <ybzadough@gmail.com>
2024-05-04 14:31:54 -0400

    remove unused per_page

--- src/data/routes.ts
+++ src/data/routes.ts
@@ -4,7 +4,7 @@ const eventsApiRoute = `${baseRoute}/tribe/events/v1/events`;
 
 export const routes = {
   boardMembers: `${wpApiRoute}/board_members?per_page=100`,
-  events: `${eventsApiRoute}?per_page=10&status=publish&starts_after=1900-01-01&per_page=1000000000`,
+  events: `${eventsApiRoute}?status=publish&starts_after=1900-01-01&per_page=1000000000`,
   pageSpoilers: `${wpApiRoute}/page-spoiler`,
   carouselAds: `${wpApiRoute}/carousel-ad?_embed`,
   contactInfo: `${wpApiRoute}/cba-contact-infos`,
